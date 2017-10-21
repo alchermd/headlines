7c3edfb8971331c0058ce6426e10239f57cbfc97
alchermd <johnalcherdoloiras@gmail.com>
2017-10-22 03:27:43 +0800

    Create dynamic routing for supported sources.

--- app.py
+++ app.py
@@ -4,14 +4,22 @@ from flask import Flask, render_template
 app = Flask(__name__, instance_relative_config=True)
 app.config.from_pyfile("appconfig.py")
 
-BBC_id= "bbc-news"
+sources = {
+    "bbc": "bbc-news",
+    "cnn": "cnn",
+    "hackernews": "hacker-news"
+}
+
+def create_link(source):
+    if source in sources.keys():
+        return f"https://newsapi.org/v1/articles?source={sources[source]}&sortBy=top&apiKey={app.config['API_KEY']}"
+    
 
 @app.route("/")
-def index():
-    r = requests.get(
-        f"https://newsapi.org/v1/articles?source={BBC_id}&sortBy=top&apiKey={app.config['API_KEY']}"
-    )
-    return render_template("index.html", articles=r.json().get("articles"))
+@app.route("/<source>")
+def index(source="bbc"):
+    r = requests.get(create_link(source))
+    return render_template("index.html", articles=r.json().get("articles"), source=source)
 
 
 if __name__ == "__main__":
