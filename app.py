257686cfa72318c0b476d4623731080f848c4942
alchermd <johnalcherdoloiras@gmail.com>
2017-10-22 03:08:43 +0800

    Use instance folder to load configuration file.

--- app.py
+++ app.py
@@ -1,15 +1,15 @@
 import requests
 from flask import Flask, render_template
 
-app = Flask(__name__)
+app = Flask(__name__, instance_relative_config=True)
+app.config.from_pyfile("appconfig.py")
 
 BBC_id= "bbc-news"
-API_KEY = "c4002216fa5446d582b5f31d73959d36"
 
 @app.route("/")
 def index():
     r = requests.get(
-        f"https://newsapi.org/v1/articles?source={BBC_id}&sortBy=top&apiKey={API_KEY}"
+        f"https://newsapi.org/v1/articles?source={BBC_id}&sortBy=top&apiKey={app.config['API_KEY']}"
     )
     return render_template("index.html", articles=r.json().get("articles"))
 
