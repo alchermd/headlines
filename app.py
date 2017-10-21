73949aa629076b4b25d78ee52c23b302e3e13bce
alchermd <johnalcherdoloiras@gmail.com>
2017-10-22 00:48:32 +0800

    Serve BBC's RSS feed on the index page.

--- app.py
+++ app.py
@@ -1,10 +1,14 @@
-from flask import Flask
+import feedparser
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
+BBC_FEED = "http://feeds.bbci.co.uk/news/rss.xml"
+
 @app.route("/")
 def index():
-    return "no news is good news :)"
+    feed = feedparser.parse(BBC_FEED)
+    return render_template("index.html", feed=feed.get('entries'))
 
 
 if __name__ == "__main__":
