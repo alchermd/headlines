bcc455591d8b4f3f01b39a0ec97114e84e192ff0
alchermd <johnalcherdoloiras@gmail.com>
2017-10-22 01:02:56 +0800

    Show headlines from BBC.

--- app.py
+++ app.py
@@ -3,13 +3,13 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
-BBC_FEED = "http://feeds.bbci.co.uk/news/rss.xml"
+BBC_id= "bbc-news"
 API_KEY = "c4002216fa5446d582b5f31d73959d36"
 
 @app.route("/")
 def index():
     r = requests.get(
-        f"https://newsapi.org/v1/articles?source=the-next-web&sortBy=latest&apiKey={API_KEY}"
+        f"https://newsapi.org/v1/articles?source={BBC_id}&sortBy=top&apiKey={API_KEY}"
     )
     return render_template("index.html", articles=r.json().get("articles"))
 
