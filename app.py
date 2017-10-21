91ea026fa0c354c81cf0a1e52dbbe626b83a00f8
alchermd <johnalcherdoloiras@gmail.com>
2017-10-22 00:56:05 +0800

    Use requests instead of feedparser.

--- app.py
+++ app.py
@@ -1,14 +1,17 @@
-import feedparser
+import requests
 from flask import Flask, render_template
 
 app = Flask(__name__)
 
 BBC_FEED = "http://feeds.bbci.co.uk/news/rss.xml"
+API_KEY = "c4002216fa5446d582b5f31d73959d36"
 
 @app.route("/")
 def index():
-    feed = feedparser.parse(BBC_FEED)
-    return render_template("index.html", feed=feed.get('entries'))
+    r = requests.get(
+        f"https://newsapi.org/v1/articles?source=the-next-web&sortBy=latest&apiKey={API_KEY}"
+    )
+    return render_template("index.html", articles=r.json().get("articles"))
 
 
 if __name__ == "__main__":
