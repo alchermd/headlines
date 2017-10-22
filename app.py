10ea9d7e542807e064e36b5f9ce8bccd32990fb4
alchermd <johnalcherdoloiras@gmail.com>
2017-10-22 16:33:00 +0800

    Use GET request arguments to determine the news source.

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 import requests
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__, instance_relative_config=True)
 app.config.from_pyfile("appconfig.py")
@@ -10,14 +10,19 @@ sources = {
     "hackernews": "hacker-news"
 }
 
+
 def create_link(source):
     if source in sources.keys():
         return f"https://newsapi.org/v1/articles?source={sources[source]}&sortBy=top&apiKey={app.config['API_KEY']}"
     
 
 @app.route("/")
-@app.route("/<source>")
-def index(source="bbc"):
+def index():
+    query = request.args.get("source")
+    if query is None or query.lower() not in sources.keys():
+        source = "bbc"
+    else:
+        source = query.lower()
     r = requests.get(create_link(source))
     return render_template("index.html", articles=r.json().get("articles"), source=source)
 
